d949e0d3487306dd31a11b2deab22576b39b558a
Evgeny Lityushkin <tcifrovik@gmail.com>
2022-09-18 21:05:40 +0300

    Правки

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -100,9 +100,9 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.environ.get('DB_NAME', 'fooddb'),
-        'USER': os.environ.get('POSTGRES_USER', 'fooduser'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '123456'),
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432')
     }
